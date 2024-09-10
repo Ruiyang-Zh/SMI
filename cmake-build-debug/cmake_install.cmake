0485e4c357be14f489fe2d2ce5f4c0ebecea9d53
221820034-Zhang Ruiyang <221820034@smail.nju.edu.cn>
2024-09-10 11:48:25 +0800

    Ignore Miscellaneous

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/ProgrammingFiles/myProjects/C/CLionProjects/SQL
+# Install script for directory: D:/Codespace/Code/C/SQL
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "D:/JetBrains/CLion/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "D:/Apps/Jetbrains/CLion/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/ProgrammingFiles/myProjects/C/CLionProjects/SQL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Codespace/Code/C/SQL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
